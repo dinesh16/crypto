204cbb56ff3adae280c8d38442734e5373346342
Dinesh Anthony <anthony.dinesh16@gmail.com>
2021-10-10 01:04:42 +0100

    update specs to cryptocurrency service class

--- spec/services/cryptocurrency_spec.rb
+++ spec/services/cryptocurrency_spec.rb
@@ -1,17 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Nomics::Cryptocurrency, type: :model do
-  describe 'Nomics api Cryptocurrency class' do
-    # subject { described_class.new }
+  describe 'Nomics api Cryptocurrency currencies/ticker' do
 
-    let(:params) { { ids: ['BTC', 'XRP']} }
-    let(:endpoint) { '/currencies/ticker' }
-    let(:service) { described_class.new(token) }
+    let(:ids) { ['BTC', 'XRP'] }
+    let(:params) {{ ids: ids.join(',') }}
+    let(:service) { described_class.new(params) }
 
-    it 'nomics ddd call', :vcr do
-      binding.pry
-      response = service.call(endpoint, params)
-      expect(response.status).to eq(200)
+    describe 'currencies_by_ids', :vcr do
+      before do
+        @response = service.currencies_by_ids
+        @result = @response.parse
+      end
+
+      it 'expect successful call for currencies_by_ids and BTC and XRP on the result' do
+        expect(@response.status).to eq(200)
+
+        expect(@result.count).to eq(ids.count)
+        expect(@result.pluck("id")).to eq(ids)
+
+        btc = @result.select {|res| res['id'] == 'BTC' }.first
+        expect(btc['symbol']).to eq('BTC')
+        expect(btc['price']).to be_present
+        expect(btc['status']).to eq('active')
+        expect(btc['circulating_supply']).to be_present
+      end
     end
+
   end
 end
